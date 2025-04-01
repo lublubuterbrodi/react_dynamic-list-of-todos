4fedef5c2f1feef998c4d75ff6b0eb8b37bcd389
lublubuterbrodi <lublubuterbrodi@gmail.com>
2025-04-01 17:34:28 +0300

    add task solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,30 +1,69 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select data-cy="statusSelect">
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+import React, { ChangeEvent } from 'react';
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
+type Props = {
+  filterStatus: string;
+  setFilterStatus: (status: string) => void;
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+};
 
-      <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
-      </span>
-    </p>
-  </form>
-);
+export const TodoFilter: React.FC<Props> = ({
+  filterStatus,
+  setFilterStatus,
+  searchQuery,
+  setSearchQuery,
+}) => {
+  return React.createElement(
+    'form',
+    { className: 'field has-addons' },
+    React.createElement(
+      'p',
+      { className: 'control' },
+      React.createElement(
+        'span',
+        { className: 'select' },
+        React.createElement(
+          'select',
+          {
+            'data-cy': 'statusSelect',
+            value: filterStatus,
+            onChange: (event: ChangeEvent<HTMLSelectElement>) =>
+              setFilterStatus(event.target.value),
+          },
+          React.createElement('option', { value: 'all' }, 'All'),
+          React.createElement('option', { value: 'active' }, 'Active'),
+          React.createElement('option', { value: 'completed' }, 'Completed'),
+        ),
+      ),
+    ),
+    React.createElement(
+      'p',
+      { className: 'control is-expanded has-icons-left has-icons-right' },
+      React.createElement('input', {
+        'data-cy': 'searchInput',
+        type: 'text',
+        className: 'input',
+        placeholder: 'Search...',
+        value: searchQuery,
+        onChange: (event: ChangeEvent<HTMLInputElement>) =>
+          setSearchQuery(event.target.value),
+      }),
+      React.createElement(
+        'span',
+        { className: 'icon is-left' },
+        React.createElement('i', { className: 'fas fa-magnifying-glass' }),
+      ),
+      searchQuery &&
+        React.createElement(
+          'span',
+          { className: 'icon is-right', style: { pointerEvents: 'all' } },
+          React.createElement('button', {
+            'data-cy': 'clearSearchButton',
+            type: 'button',
+            className: 'delete',
+            onClick: () => setSearchQuery(''),
+          }),
+        ),
+    ),
+  );
+};
